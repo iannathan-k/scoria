142094e7b34bacfbbcc56dbf5ec2f19d9e4366e7
iannathan-k <ianmtv@gmail.com>
2025-05-17 17:12:29 +0700

    version fix

--- src/core/Command.java
+++ src/core/Command.java
@@ -32,7 +32,7 @@ public class Command {
 
     private static final String uci_string = 
         """
-        id name Scoria_v3.7.44
+        id name Scoria_v3.7.45
         id author iannathan-k (Ian Nathan Kusmiantoro)
 
         option name Debug Log File type string default <empty>
@@ -52,7 +52,7 @@ public class Command {
         /____/\\___/\\____/_/  /_/\\__,_/  
         
         Ian Nathan Kusmiantoro
-        Version 3.7.44
+        Version 3.7.45
     ========================================
     """;
 
